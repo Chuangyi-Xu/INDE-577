953aefe86c15298950a26ce6399bd8b775a7c519
Chuangyi-Xu <cx35@rice.edu>
2025-12-13 19:31:36 -0600

    final check1

--- src/rice_ml/__init__.py
+++ src/rice_ml/__init__.py
@@ -1,62 +1,14 @@
-"""
-rice_ml: A from-scratch machine learning library
-Developed for INDE 577 / CMOR 438 at Rice University
-"""
-
-# Supervised Learning
-from .perceptron import Perceptron
-from .logistic_regression import LogisticRegression
+# Core linear models
 from .linear_regression import LinearRegression
-from .knn import KNN
-from .decision_trees import DecisionTreeClassifier, DecisionTreeRegressor
-from .random_forest import RandomForestClassifier, RandomForestRegressor
-from .multilayer_perceptron import MultilayerPerceptron
-
-# Unsupervised Learning
-from .kmeans import KMeans
-from .dbscan import DBSCAN
-from .pca import PCA
-from .label_propagation import LabelPropagation
-from .community_detection import CommunityDetection
+from .logistic_regression import LogisticRegression
+from .perceptron import Perceptron
 
 # Utilities
 from .distance_metrics import (
     euclidean_distance,
     manhattan_distance,
-    cosine_distance
+    cosine_distance,
 )
 
-# Ensemble methods
-from .ensemble_methods import (
-    BaggingClassifier,
-    VotingClassifier
-)
-
-__all__ = [
-    # Supervised
-    "Perceptron",
-    "LogisticRegression",
-    "LinearRegression",
-    "KNN",
-    "DecisionTreeClassifier",
-    "DecisionTreeRegressor",
-    "RandomForestClassifier",
-    "RandomForestRegressor",
-    "MultilayerPerceptron",
-
-    # Unsupervised
-    "KMeans",
-    "DBSCAN",
-    "PCA",
-    "LabelPropagation",
-    "CommunityDetection",
-
-    # Utilities
-    "euclidean_distance",
-    "manhattan_distance",
-    "cosine_distance",
-
-    # Ensemble
-    "BaggingClassifier",
-    "VotingClassifier",
-]
+# Meta models
+from .ensemble_methods import VotingClassifier
